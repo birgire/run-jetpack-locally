5db280ac81db3c08f7ae2ec2f374e949dd05bd12
Birgir Erlendsson <birgire@gmail.com>
2014-06-27 20:33:53 +0000

    Update run-jetpack-locally.php

--- run-jetpack-locally.php
+++ run-jetpack-locally.php
@@ -3,7 +3,8 @@
  * Plugin Name: Run JetPack locally
  * Plugin URI:  http://wordpress.stackexchange.com/a/144317/26350
  * Author:      birgire
- * Version:     0.0.1
+ * Version:     0.0.2
  */
 
-add_filter( 'jetpack_development_mode', '__return_true' );
+// Activate the development mode:
+add_filter( 'jetpack_development_mode', '__return_true', PHP_INT_MAX );
